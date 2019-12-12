bbc50f48bd2df4fc12456bd599cd2cc4e4564493
poxu <poxvuibr@gmail.com>
2019-12-12 19:15:17 +0300

    Add unit test for query demonstration

---
+++ src/main/java/com/evilcorp/demo/entity/User.java
@@ -0,0 +1,47 @@
+package com.evilcorp.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private UUID userId = UUID.randomUUID();
+    @Column(name = "name")
+    private String name;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return userId.equals(user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+}
--- src/test/java/com/evilcorp/demo/TestcontainersSpringDemoApplicationTests.java
+++ src/test/java/com/evilcorp/demo/TestcontainersSpringDemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.evilcorp.demo;
 
+import com.evilcorp.demo.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.PostConstruct;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class TestcontainersSpringDemoApplicationTests {
+	@Autowired
+	UserRepository userRepository;
+	private User createdUser;
 
-	@Test
-	void contextLoads() {
+	@PostConstruct
+	void setUp() {
+		createdUser = new User();
+		createdUser.setName("Fry");
+		userRepository.save(createdUser);
 	}
 
+	@Test
+	void userRepositoryLoaded() {
+		assertNotNull(userRepository);
+	}
+
+	@Test
+	void userAdded() {
+		final Optional<User> loadedUser = userRepository.findById(createdUser.getUserId());
+		assertTrue(loadedUser.isPresent());
+		assertEquals("Fry", loadedUser.get().getName());
+		assertNotSame(createdUser, loadedUser.get());
+	}
 }
