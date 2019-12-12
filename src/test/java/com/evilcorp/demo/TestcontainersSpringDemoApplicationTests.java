7bcd4a0e85541017ddd0ecfd87bdc3a37bb8e2da
poxu <poxvuibr@gmail.com>
2019-12-12 18:35:21 +0300

    Project, made with start.spring.io

---
+++ src/test/java/com/evilcorp/demo/TestcontainersSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evilcorp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
